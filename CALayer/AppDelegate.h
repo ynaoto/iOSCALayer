5003944746288f14f4eaa7228546ff734ba1f702
Naoto Yoshioka <yos@pb3.so-net.ne.jp>
2013-11-14 14:33:09 +0900

    -n Initial Commit

---
+++ CALayer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CALayer
+//
+//  Created by Naoto Yoshioka on 2013/11/14.
+//  Copyright (c) 2013年 Naoto Yoshioka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CALayer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CALayer
+//
+//  Created by Naoto Yoshioka on 2013/11/14.
+//  Copyright (c) 2013年 Naoto Yoshioka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
